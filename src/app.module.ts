c7cb77732472c740e542211349617be349e13e6b
adrianusyoga1p <adrianusyoga26@gmail.com>
2025-07-03 21:39:34 +0700

    feat: add new module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,18 @@ import { RoleModule } from './api/role/role.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './api/users/users.module';
 import { BusinessModule } from './api/business/business.module';
+import { WarehouseModule } from './api/warehouse/warehouse.module';
+import { ProductModule } from './api/product/product.module';
 
 @Module({
-  imports: [AuthModule, RoleModule, PrismaModule, UsersModule, BusinessModule],
+  imports: [
+    AuthModule,
+    RoleModule,
+    PrismaModule,
+    UsersModule,
+    BusinessModule,
+    WarehouseModule,
+    ProductModule,
+  ],
 })
 export class AppModule {}
