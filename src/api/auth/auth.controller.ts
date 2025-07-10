35670b229a2d147c1d5b5b121cdc6d1fd45eaffe
adrianusyoga1p <adrianusyoga26@gmail.com>
2025-07-10 21:58:10 +0700

    feat: auth public akses

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -1,12 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from 'src/dto';
+import { Public } from 'src/decotator/public.decotator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   // post auth/login
+  @Public()
   @Post('login')
   login(@Body() dto: LoginDto) {
     return this.authService.login(dto);
